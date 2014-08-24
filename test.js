733f02d1cc0bab8e6d319d213ed8143ce08a88d6
Peter Steinberg <objectliteral@outlook.com>
2014-08-24 17:54:49 +0200

    Added tests for the `off` method's new behavior.

--- test.js
+++ test.js
@@ -184,12 +184,121 @@ describe('`off` function', function () {
     }
   });
 
-  it('should result in the given callback no longer being triggered', function () {
+  it('should result in the given callback no longer being triggered when called with an event type and a function reference', function () {
     var channel = Events({}),
         f = function () { assert.fail(undefined, undefined, 'This function must not be executed.'); };
     channel.on('ping', f);
     channel.off('ping', f);
     channel.emit('ping');
+  });
+
+  it('should result in callbacks for the given type no longer being triggered if called with an event type', function () {
+    var channel = Events({}),
+        f = function () { assert.fail(undefined, undefined, 'This function must not be executed.'); },
+        f2 = function () { assert.fail(undefined, undefined, 'This function must not be executed.'); };
+    channel.on('ping', f);
+    channel.on('ping', f2);
+    channel.off('ping');
+    channel.emit('ping');
+  });
+
+  it('should result in no more callbacks being triggered if called with no arguments', function () {
+    var channel = Events({}),
+        f = function () { assert.fail(undefined, undefined, 'This function must not be executed.'); },
+        f2 = function () { assert.fail(undefined, undefined, 'This function must not be executed.'); };
+    channel.on('ping', f);
+    channel.on('pong', f2);
+    channel.off();
+    channel.emit('ping');
+    channel.emit('pong');
+  });
+
+  it('should not affect any other function except for the given one when called with an event type and a function reference', function () {
+    var channel = Events({}),
+        f = function () { assert.fail(undefined, undefined, 'This function must not be executed.'); },
+        f2 = function () { assert(true); };
+    channel.on('ping', f);
+    channel.on('ping', f2);
+    channel.off('ping', f);
+    channel.emit('ping');
+  });
+
+  it('should not affect any other events other than the given on if called with an event type', function () {
+    var channel = Events({}),
+        f = function () { assert.fail(undefined, undefined, 'This function must not be executed.'); },
+        f2 = function () { assert(true); };
+    channel.on('ping', f);
+    channel.on('pong', f2);
+    channel.off('ping');
+    channel.emit('ping');
+    channel.emit('pong');
+  });
+
+  it('should not affect any other channels other than the given one if called with no arguments', function () {
+    var channel = Events({}),
+        channel2 = Events({}),
+        f = function () { assert.fail(undefined, undefined, 'This function must not be executed.'); },
+        f2 = function () { assert(true); };
+    channel.on('ping', f);
+    channel2.on('ping', f2);
+    channel.off();
+    channel.emit('ping');
+    channel2.emit('ping');
+  });
+
+  it('should not affect locked events when called with an event type and a function reference', function () {
+    var channel = Events({}),
+        res = false,
+        f = function () { res = true};
+    channel.on('ping', f);
+    channel.lock('ping');
+    channel.off('ping', f);
+    channel.emitSync('ping');
+    assert.equal(true, res);
+  });
+
+   it('should not affect events on locked channels when called with an event type and a function reference', function () {
+    var channel = Events({}),
+        res = false,
+        f = function () { res = true};
+    channel.on('ping', f);
+    channel.lock();
+    channel.off('ping', f);
+    channel.emitSync('ping');
+    assert.equal(true, res);
+  });
+
+   it('should not affect locked events when called with an event type', function () {
+    var channel = Events({}),
+        res = false,
+        f = function () { res = true};
+    channel.on('ping', f);
+    channel.lock('ping');
+    channel.off('ping');
+    channel.emitSync('ping');
+    assert.equal(true, res);
+  });
+
+   it('should not affect events on locked channels when called with an event type', function () {
+    var channel = Events({}),
+        res = false,
+        f = function () { res = true};
+    channel.on('ping', f);
+    channel.lock();
+    channel.off('ping');
+    channel.emitSync('ping');
+    assert.equal(true, res);
+  });
+
+   it('should not affect locked channels when called with no arguments', function () {
+    var channel = Events({}),
+        res = false,
+        f = function () { res = true};
+    channel.on('ping', f);
+    channel.lock();
+    channel.off();
+    channel.emitSync('ping');
+    assert.equal(true, res);
   });
 
 });
