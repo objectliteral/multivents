a7a05522461747cb15e72840bb3835213579a1fe
objectliteral <objectliteral@outlook.com>
2017-03-06 21:14:49 +0100

    Changed gulpfile to now write all build results into dist folder

--- gulpfile.js
+++ gulpfile.js
@@ -8,37 +8,37 @@ var gulp = require('gulp'),
     gzip = require('gulp-gzip');
 
 gulp.task('lint', function () {
-    gulp.src('./multivents.js')
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
+    return gulp.src('./multivents.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('pre-test', function () {
-    gulp.src('./multivents.js')
-    .pipe(istanbul())
-    .pipe(istanbul.hookRequire());
+    return gulp.src('./multivents.js')
+        .pipe(istanbul())
+        .pipe(istanbul.hookRequire());
 });
 
 gulp.task('test', [ 'pre-test' ], function () {
-    gulp.src('./tests/*.js')
-      .pipe(mocha({ reporter: 'progress' }))
-      .pipe(istanbul.writeReports({ reporters: [ 'text', 'html' ] }));
+    return gulp.src('./tests/*.js')
+        .pipe(mocha({ reporter: 'progress' }))
+        .pipe(istanbul.writeReports({ reporters: [ 'text', 'html' ] }));
 });
 
 gulp.task('build', [], function () {
-    gulp.src('./multivents.js')
-      .pipe(rename('multivents.min.js'))
-      .pipe(sourcemaps.init())
-        .pipe(uglify())
-      .pipe(sourcemaps.write('./', {includeContent:false}))
-      .pipe(gulp.dest('./'));
+    return gulp.src('./multivents.js')
+        .pipe(rename('multivents.min.js'))
+        .pipe(sourcemaps.init())
+            .pipe(uglify())
+        .pipe(sourcemaps.write('./', {includeContent:false}))
+        .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('compress', [], function () {
-    gulp.src('./multivents.min.js')
+gulp.task('compress', [ 'build' ], function () {
+    return gulp.src('./dist/multivents.min.js')
         .pipe(gzip())
-        .pipe(gulp.dest('.'));
+        .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('default', [ 'lint', 'test', 'build', 'compress' ]);
