74a8ca82b9d1c29df4f2ed6479cdcd1f2612a9b1
Peter Steinberg <objectliteral@outlook.com>
2018-08-09 17:48:04 +0200

    Added more tests for the catchall feature

--- tests/catchall.js
+++ tests/catchall.js
@@ -4,7 +4,7 @@ var assert = require('assert'),
 describe('catchall type',  function () {
 
 	it('should result in all callbacks for all types being invoked when emitting *',  function () {
-        var channel = Channel({}),
+        var channel = Channel(),
         	check = 0,
             f = function () { check++; };
         channel.on('ping', f);
@@ -15,7 +15,7 @@ describe('catchall type',  function () {
     });
 
     it('should result in a callback being called for all types when being registered for *', function () {
-    	var channel = Channel({}),
+    	var channel = Channel(),
         	check = 0,
             f = function () { check++; };
         channel.on('*', f);
@@ -23,6 +23,48 @@ describe('catchall type',  function () {
         channel.emitSync('pong');
         channel.emitSync('peng');
         assert.equal(check, 3)
-    })
+    });
+
+    it('should not execute silenced event types when emitting *', function () {
+    	var channel = Channel({}),
+            f = function () { assert.fail(); };
+        channel.on('ping', f);
+        channel.silence('ping');
+        channel.emitSync('*');
+    });
+
+    it('should not execute silenced callbacks when emitting *', function () {
+    	var channel = Channel(),
+            f = function () { assert.fail(); };
+        channel.on('ping', f);
+        channel.silence('ping', f);
+        channel.emitSync('*');
+    });
+
+    it('should not execute callbacks on silenced channels when emitting *', function () {
+    	var channel = Channel(),
+            f = function () { assert.fail(); };
+        channel.on('ping', f);
+        channel.silence();
+        channel.emitSync('*');
+    });
+
+    it('should not register callbacks on locked channels when registering for *', function () {
+    	var channel = Channel();
+    	channel.lock();
+    	channel.on('*', function () {
+    		assert.fail();
+    	});
+    	channel.emit('ping');
+    });
+
+    it('should not execute callbacks on silenced channels when registering for *', function () {
+    	var channel = Channel();
+    	channel.on('*', function () {
+    		assert.fail();
+    	});
+    	channel.silence();
+    	channel.emit('ping');
+    });
 
 });
